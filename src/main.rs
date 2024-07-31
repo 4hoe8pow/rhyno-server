933bc7cf7c0fe9a2ed1fcb91c78cdd960cd70a4a
4hoe8pow <tokunaga@agni.ninja>
2024-07-31 23:20:11 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
-use axum::{routing::get, Router};
+use axum::{extract::State, routing::get, Router};
+use qdrant_client::Qdrant;
+use std::sync::Arc;
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+struct AppState {
+    qdrant: Qdrant,
+}
+
+async fn list_collections(State(state): State<Arc<AppState>>) -> String {
+    format!("{:?}\n", state.qdrant.list_collections().await)
 }
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+async fn main(
+    #[shuttle_qdrant::Qdrant(cloud_url = "{secrets.CLOUD_URL}", api_key = "{secrets.API_KEY}")]
+    qdrant: Qdrant,
+) -> shuttle_axum::ShuttleAxum {
+    let state = Arc::new(AppState { qdrant });
+
+    let router = Router::new()
+        .route("/", get(list_collections))
+        .with_state(state);
 
     Ok(router.into())
 }
